0af51b557359da5fcb63ebd4f8d6adeb71382e89
arudraMS <82988386+arudraMS@users.noreply.github.com>
2023-02-11 00:41:51 -0600

    Update clean_up.sh

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -26,7 +26,7 @@ set -o nounset
 # PARAMETERS
 #
 # RESOURCE_GROUP_NAME_PREFIX
-prefix="mdwdops-${DEPLOYMENT_ID}"
+prefix="mdwdops"
 
 echo "!! WARNING: !!"
 echo "THIS SCRIPT WILL DELETE RESOURCES PREFIXED WITH $prefix !!"
